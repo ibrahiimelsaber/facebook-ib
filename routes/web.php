7a6170487839196a54aef9ddb461abd6b81f7a3b
Ibrahim Elsaber <ibrahimelsaber8@gmail.com>
2021-08-19 20:03:31 +0200

    intialization
    install required dependancies

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\AppController;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('{any}', [AppController::class, 'index'])
+    ->where('any','.*')
+    ->middleware('auth')
+    ->name('home');
